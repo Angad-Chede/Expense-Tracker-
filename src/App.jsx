03acb4abd751310bfd46d0cc88dd1da8941e4b91
Angad-Chede <angadchede30@gmail.com>
2026-03-07 00:49:12 +0530

    import Currency and path was added!!

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard.jsx";
 import Login from "./components/login.jsx";
 import Signup from "./components/Signup.jsx";
+import CurrencyExchanger from "./components/CurrencyExchanger";
 
 import "./assets/css/style.css";
 
@@ -15,6 +16,8 @@ function App() {
         <Route path="/login" element={<Login />} />
 
         <Route path="/signup" element={<Signup />} />
+
+        <Route path="/currency" element={<CurrencyExchanger />} />
 
       </Routes>
     </BrowserRouter>
